b974f15f9bd1bfcb86097e925ab2ae0c82003ca3
Chris <audionautic@gmail.com>
2022-01-10 18:25:40 -0800

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="re-wx",
-    version="0.00.07",
+    version="0.00.08",
     author="Chris Kiehl",
     author_email="me@chriskiehl.com",
     description="A library for building modern declarative desktop applications in WX",
