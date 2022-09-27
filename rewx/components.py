c1d53817d9c2f5a50cf58bf589f6e1a9340eb838
James Brock <jamesbrock@gmail.com>
2022-09-27 18:23:26 +0900

    Add DirPickerCtrl

--- rewx/components.py
+++ rewx/components.py
@@ -18,6 +18,7 @@ CheckBox = wx.CheckBox
 # CollapsiblePane = wx.CollapsiblePane
 ComboBox = wx.ComboBox
 Dropdown = ComboBox
+DirPickerCtrl = wx.DirPickerCtrl
 FilePickerCtrl = wx.FilePickerCtrl
 Frame = wx.Frame
 Gauge = wx.Gauge
